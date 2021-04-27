d775e2ba72afc8513b7cff49e8facb85db5f8872
Hidde Wieringa <hidde@hiddewieringa.nl>
2021-04-27 22:03:39 +0200

    Fix removed but still used code

--- scripts/bounds.py
+++ scripts/bounds.py
@@ -7,6 +7,10 @@ import mapnik
 
 import environment
 
+EPSG_4326 = mapnik.Projection('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')
+EPSG_3857 = mapnik.Projection('+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs +over')
+latitudeLongitudeToWebMercator = mapnik.ProjTransform(EPSG_4326, EPSG_3857)
+
 ORIENTATION_LANDSCAPE = 'landscape'
 ORIENTATION_PORTRAIT = 'portrait'
 
